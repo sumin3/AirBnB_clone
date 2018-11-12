6733af5405dfa438f63df37a16a9b38f88a2bbc5
sumin3 <suminyu@yahoo.com>
2018-11-12 03:19:35 +0000

    change private attr to private class attr, use className as the key, fix save and reload function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,58 +1,56 @@
+#!/usr/bin/python3
+""" FileStorage module """
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
-
-    def __init__(self, *args, **kwargs):
-        # import pdb; pdb.set_trace()
-        self.file_path = kwargs['__file_path']
-        self.__objects = {}
+    """ FileStorage class
+    Attributes:
+        __objects: dictionary - empty but will store all
+                   objects by <class name>.id.
+        __file_path: string - path to the JSON file.
+    """
+    __objects = {}
+    __file_path = "file.json"
 
     def all(self):
-        """returns the dictionary __objects"""
-        return self.objects
-
-    @property
-    def file_path(self):
-        return self.__file_path
-
-    @file_path.setter
-    def file_path(self, value):
-        self.__file_path = value
-        # try:
-        #     with open(value, mode="r", encoding="utf-8"):
-        # except FileNotFoundError:
-        #     pass
-
-    @property
-    def objects(self):
+        """ all method: returns the dictionary __objects
+        Return:
+            return dictionary __objects
+        """
         return self.__objects
 
-    # @objects.setter
-    # def objects(self, value):
-    #     self.__objects = 
-
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id"""
-        self.objects[
-            '{}.{}'.format(obj.__class__, obj.id)
-        ] = obj
+        """ new method: sets in __objects the obj with
+        key <obj class name>.id.
+        Args:
+            obj: the new object that want to create
+        """
+        self.__objects['{}.{}'.format(
+            obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
+        """ sace method: serializes __objects to the JSON
+        file (path: __file_path)
+        """
         try:
-            with open(self.file_path, mode="w+", encoding="utf-8") as f:
-                json.dump(self.objects, f)
+            with open(self.__file_path, mode="w", encoding="utf-8") as f:
+                d = {k: v.to_dict() for k, v in self.__objects.items()}
+                f.write(json.dumps(d))
         except FileNotFoundError:
             pass
 
     def reload(self):
-        """ deserializes the JSON file to __objects
-         (only if the JSON file (__file_path) exists ; otherwise, do nothing.
-         If the file doesn’t exist, no exception should be raised) """
+        """ reload method: deserializes the JSON file to __objects
+         (only if the JSON file (__file_path) exists ; otherwise,
+        do nothing. If the file doesn’t exist, no exception should
+        be raised)
+        """
         try:
-            with open(self.file_path, mode="r", encoding="utf-8") as f:
-                for obj in json.load(f):
-                    self.new(obj)
+            with open(self.__file_path, mode="r", encoding="utf-8") as f:
+                d = json.load(f)
+                for k, v in d.items():
+                    self.__class__.__objects[k] = BaseModel(**v)
         except FileNotFoundError:
             pass
