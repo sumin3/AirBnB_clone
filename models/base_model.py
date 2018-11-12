34070571edb29b708afe5c5d7b106706b03f07d1
sumin3 <suminyu@yahoo.com>
2018-11-12 07:06:03 +0000

    in to dict method, use copy of self.__dict__ instead

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of instance"""
-        retval = self.__dict__
+        retval = self.__dict__.copy()
         retval["__class__"] = self.__class__.__name__
         retval["created_at"] = self.created_at.isoformat()
         retval["updated_at"] = self.updated_at.isoformat()
