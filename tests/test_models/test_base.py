f3205d3f7a08a5668b567d7efb2445caaa048dc8
Alex Farioletti <gsf.nyc@gmail.com>
2018-11-13 12:45:30 -0800

    just floats

--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -14,15 +14,7 @@ class TestBaseModel(unittest.TestCase):
         self.model2 = BaseModel()
         self.model2.name = "Betty"
         self.model2.my_number = 98
-        self.model1.list = []
-        self.model1.dict = {'1': 1}
-        self.model1.set = {1, 2, 3}
-        self.model1.truth = True
-        self.model1.none = None
         self.model1.float = 1.1
-        self.model1.nan = float('nan')
-        self.model1.inf = float('inf')
-        self.model1.tuple = (1, 2, 3)
 
     def test_id(self):
         """id is string
@@ -77,15 +69,4 @@ class TestBaseModel(unittest.TestCase):
         """
         """model1.updated_at: type string"
         """
-
-
-        self.assertEqual(type(self.model1.list), list)
-        self.assertEqual(type(self.model1.dict), dict)
-        self.assertEqual(type(self.model1.set), set)
-        self.assertEqual(type(self.model1.truth), bool)
-        self.assertEqual(type(self.model1.none), None)
         self.assertEqual(type(self.model1.float), float)
-        self.assertEqual(type(self.model1.tuple), tuple)
-        
-        self.assertEqual(self.model1.nan), nan)
-        self.assertEqual(self.model1.inf), inf)
