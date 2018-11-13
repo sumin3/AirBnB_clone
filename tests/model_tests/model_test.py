7bc85d7a560127c11d9bcafe81c510b87942e36c
Alex Farioletti <gsf.nyc@gmail.com>
2018-11-13 11:00:24 -0800

    test start

---
+++ tests/model_tests/model_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""BaseModel class unitests"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
