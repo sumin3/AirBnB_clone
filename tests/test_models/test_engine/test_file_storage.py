090c15689a6042089afd72e4bd75dd3743d98098
sumin3 <suminyu@yahoo.com>
2018-11-14 08:29:56 +0000

    add tear down: clear object, in test_save: open file.json, and check the size of file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,31 +9,68 @@ from datetime import datetime
 
 
 class TestFileStorage(unittest.TestCase):
-    """ class Base parent class unit tests
+    """ class FileStorage class unit tests
     """
 
     def setUp(self):
+        """set up """
         self.storage = FileStorage()
 
-    def test_storage_objects(self):
+    def tearDown(self):
+        """ teardown tests """
+        all_objs = storage.all()
+        all_objs.clear()
+        storage.save()
+
+    def test_storage_objects_dict_datatype(self):
         """ storage.__objects is a dict """
         self.assertIsInstance(self.storage.all(), dict)
 
+    def test_storage_objects(self):
+        """ test size of objects after creates a new instance
+        """
+        pre_objs_size = len(self.storage.all())
+        base = BaseModel()
+        base.name = "Holberton"
+        post_objs_size = len(self.storage.all())
+        self.assertGreater(post_objs_size, pre_objs_size)
+        self.assertEqual(post_objs_size, pre_objs_size + 1)
+
     def test_new(self):
-        """ test new obj serializtion """
+        """ test new method - check create a new instance """
         base = BaseModel()
         base_key = "{}.{}".format(base.__class__.__name__, base.id)
         all_objs = self.storage.all()
         self.assertIn(base_key, all_objs)
+
         city = City()
         city_key = "{}.{}".format(city.__class__.__name__, city.id)
         all_objs = self.storage.all()
         self.assertIn(city_key, all_objs)
 
+    def test_unsave(self):
+        """ test not save obj serialization """
+        city_unsave = City()
+        unsave_key = "{}.{}".format(
+            city_unsave.__class__.__name__, city_unsave.id)
+        with open("file.json", "r") as file:
+            unsave_objs = file.read()
+        self.assertNotIn(unsave_key, unsave_objs)
+
     def test_save(self):
-        """ test save obj serializtion """
-        pre_objs = self.storage.all()
+        """ test save obj serialization """
+        city = City()
+        storage.save()
+        with open("file.json", "r") as file:
+            pre_objs = file.read()
+        pre_objs_size = len(pre_objs)
+
         base = BaseModel()
-        base.name = "Holberton"
-        post_objs = self.storage.all()
-        self.assertFalse(pre_objs == post_objs)
+        storage.save()
+        base_key = "{}.{}".format(base.__class__.__name__, base.id)
+        with open("file.json", "r") as file:
+            post_objs = file.read()
+        post_objs_size = len(post_objs)
+
+        self.assertGreater(post_objs_size, pre_objs_size)
+        self.assertIn(base_key, post_objs)
